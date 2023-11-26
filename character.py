384caab3aff64ba91a09692a79bd9882263ae80e
N8866U <willfuld04@gmail.com>
2023-11-26 16:29:37 -0500

    Adds a level variable to the character class

--- character.py
+++ character.py
@@ -1,8 +1,9 @@
 class Character:
-    def __init__(self, strn, dex, con, intl, wis, cha):
+    def __init__(self, strn, dex, con, intl, wis, cha, level):
         self.str = strn
         self.dex = dex
         self.con = con
         self.int = intl
         self.wis = wis
         self.cha = cha
+        self.level = level
