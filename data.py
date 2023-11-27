7e3c3a87c102a7750e98de079512917ce9559e57
N8866U <willfuld04@gmail.com>
2023-11-26 22:25:45 -0500

    Adds feats, armor, and races

--- data.py
+++ data.py
@@ -51,7 +51,112 @@ weapons = {"Antimatter Rifle": [0, 120, 360, 10, "Necrotic", "2-handed", 6, 8, 0
            "Warhammer": [15, 0, 5, 2, "Bludgeoning", "", 1, 8, 1, 10],
            "Whip": [2, 0, 10, 3, "Slashing", "Finesse", 1, 4, 0, 0],
            "Yklwa": [1, 10, 30, 2, "Piercing", "", 1, 8, 0, 0]}
-feats = {}
+
+feats = {"Aberrant Dragonmark": [],
+         "Actor": [],
+         "Adept of the Black Robes": [],
+         "Adept of the Red Robes": [],
+         "Adept of the White Robes": [],
+         "Agent of Order": [],
+         "Alert": [],
+         "Artificer Initiate": [],
+         "Athlete": [],
+         "Baleful Scion": [],
+         "Bountiful Luck": [],
+         "Cartomancer": [],
+         "Charger": [],
+         "Chef": [],
+         "Cohort of Chaos": [],
+         "Crossbow Expert": [],
+         "Crusher": [],
+         "Defensive Duelist": [],
+         "Divinely Favored": [],
+         "Dragon Fear": [],
+         "Dragon Hide": [],
+         "Drow High Magic": [],
+         "Dual Wielder": [],
+         "Dungeon Delver": [],
+         "Durable": [],
+         "Dwarven Fortitude": [],
+         "Eldritch Adept": [],
+         "Elemental Adept": [],
+         "Elven Accuracy": [],
+         "Ember of the Fire Giant": [],
+         "Fade Away": [],
+         "Fey Teleportation": [],
+         "Fey Touched": [],
+         "Fighting Initiate": [],
+         "Flames of Phlegethos": [],
+         "Fury of the Frost Giant": [],
+         "Gift of the Chromatic Dragon": [],
+         "Gift of the Gem Dragon": [],
+         "Gift of the Metallic Dragon": [],
+         "Grappler": [],
+         "Great Weapon Master": [],
+         "Guile of the Cloud Giant": [],
+         "Gunner": [],
+         "Healer": [],
+         "Heavily Armored": [],
+         "Heavy Armor Master": [],
+         "Infernal Constitution": [],
+         "Initiate of High Sorcery": [],
+         "Inspiring Leader": [],
+         "Keen Mind": [],
+         "Keenness of the Stone Giant": [],
+         "Knight of the Crown": [],
+         "Knight of the Rose": [],
+         "Knight of the Sword": [],
+         "Lightly Armored": [],
+         "Linguist": [],
+         "Lucky": [],
+         "Mage Slayer": [],
+         "Magic Initiate": [],
+         "Martial Adept": [],
+         "Medium Armor Master": [],
+         "Metamagic Adept": [],
+         "Mobile": [],
+         "Moderately Armored": [],
+         "Mounted Combatant": [],
+         "Observant": [],
+         "Orcish Fury": [],
+         "Outlands Envoy": [],
+         "Piercer": [],
+         "Planar Wanderer": [],
+         "Poisoner": [],
+         "Polearm Master": [],
+         "Prodigy": [],
+         "Resilient": [],
+         "Revenant Blade": [],
+         "Righteous Heritor": [],
+         "Ritual Caster": [],
+         "Rune Shaper": [],
+         "Savage Attacker": [],
+         "Scion of the Outer Planes": [],
+         "Second Chance": [],
+         "Sentinel": [],
+         "Shadow Touched": [],
+         "Sharpshooter": [],
+         "Shield Master": [],
+         "Skill Expert": [],
+         "Skilled": [],
+         "Skulker": [],
+         "Slasher": [],
+         "Soul of the Storm Giant": [],
+         "Spell Sniper": [],
+         "Squat Nimbleness": [],
+         "Squire of Solamnia": [],
+         "Strike of the Giants": [],
+         "Strixhaven Initiate": [],
+         "Strixhaven Mascot": [],
+         "Tavern Brawler": [],
+         "Telekinetic": [],
+         "Telepathic": [],
+         "Tough": [],
+         "Vigor of the Hill Giant": [],
+         "War Caster": [],
+         "Weapon Master": [],
+         "Wood Elf Magic": []}
+
 races = {"Dragonborn": [],
          "Dwarf": [],
          "Elf": [],
@@ -101,303 +206,19 @@ races = {"Dragonborn": [],
          "Hadozee": [],
          "Plasmoid": [],
          "Thri-kreen": [],
-         
-    Spelljammer: Adventures in Space
+         "Owlin": [],
+         "Lineages": [],
+         "Leonin": [],
+         "Kalashtar": [],
+         "Warforged": [],
+         "Verdan": [],
+         "Loxodon": [],
+         "Simic Hybrid": [],
+         "Vedalken": [],
+         "Feral Tiefling": [],
+         "Locathah": [],
+         "Grung": []}
 
-    Thri-kreen speak by clacking their mandibles and waving their antennae, indicating what they are thinking and feeling.
-
-    Racial Traits
-    Monstrosity, Chameleon Carapace, Darkvision, Secondary Arms, Sleepless, Thri-kreen Telepathy
-    View Thri-kreen Details
-
-Strixhaven: A Curriculum of Chaos Icon
-Strixhaven: A Curriculum of Chaos
-
-    Owlin
-    Strixhaven: A Curriculum of Chaos
-
-    Like owls, owlin are graced with feathers that make no sound when they move or fly, making it easy for them to sneak up on you in the library.
-
-    Racial Traits
-    Darkvision, Flight, Silent Feathers
-    View Owlin Details
-
-Van Richten’s Guide to Ravenloft Icon
-Van Richten’s Guide to Ravenloft
-
-    Lineages
-    Van Richten’s Guide to Ravenloft
-
-    In the Land of the Mists, power and dread lie in the simple question “What happened to me?”
-
-    Lineages
-    +2 to an Ability Score and +1 to One Other Ability Score, or, +1 to Three Different Ability Scores
-    View Lineages Details
-
-Mythic Odysseys of Theros Icon
-Mythic Odysseys of Theros
-
-    Leonin
-    Mythic Odysseys of Theros
-
-    Nomadic, lion-like humanoids who rarely interact with other peoples, having all they need in their shimmering homeland.
-
-    Racial Traits
-    +2 Constitution, +1 Strength, Darkvision, Claws,  Hunter's Instincts, Daunting Roar.
-    View Leonin Details
-    Satyr (Legacy)
-    Mythic Odysseys of Theros
-
-    Satyrs embody a fusion of humanoid civilization with the freedom of wild beasts.
-
-    Racial Traits
-    +2 Charisma, +1 Dexterity, Fey, Ram, Magic Resistance, Mirthful Leaps, Reveler.
-    View Satyr Details
-
-Eberron: Rising from the Last War Icon
-Eberron: Rising from the Last War
-
-    Changeling (Legacy)
-    Eberron: Rising from the Last War
-
-    A changeling can shift its face and form with a thought as a form of artistic and emotional expression.
-
-    Racial Traits
-    +2 Charisma, +1 to One Other Ability Score, Shapechanger, Changeling Instincts
-    View Changeling Details
-    Kalashtar
-    Eberron: Rising from the Last War
-
-    The kalashtar are a compound people created from the union of humanity and renegade spirits from the plane of dreams.
-
-    Racial Traits
-    +2 Wisdom, +1 Charisma, Dual Mind, Mental Discipline, Mind Link, Severed from Dreams
-    View Kalashtar Details
-    Shifter (Legacy)
-    Eberron: Rising from the Last War
-
-    Shifters - sometimes called the weretouched - are humanoids with a bestial aspect.
-
-    Racial Traits
-    Darkvision, Shifting
-    View Shifter Details
-    Warforged
-    Eberron: Rising from the Last War
-
-    Warforged are made from wood and metal, but they can feel pain and emotion. Built as weapons, they must now find a purpose beyond war.
-
-    Racial Traits
-    +2 Constitution, +1 to One Other Ability Score, Constructed Resilience, Sentry's Rest, Integrated Protection, Specialized Design
-    View Warforged Details
-
-Acquisitions Incorporated Icon
-Acquisitions Incorporated
-
-    Verdan
-    Acquisitions Incorporated
-
-    The verdan owe their existence to chaos - doing their best to find their way in an unfamiliar world..
-
-    Racial Traits
-    +1 Constitution, +2 Charisma, Black Blood Healing, Limited Telepathy, Persuasive, Telepathic Insight
-    View Verdan Details
-
-Guildmasters' Guide to Ravnica Icon
-Guildmasters' Guide to Ravnica
-
-    Centaur (Legacy)
-    Guildmasters' Guide to Ravnica
-
-    The upper bodies of centaurs are comparable to human torsos in size, and their lower equine bodies average about 4 feet tall at the withers. They fill similar roles as cavalry warriors, messengers, outriders, and scouts.
-
-    Racial Traits
-    +2 Strength, +1 Wisdom, Fey, Charge, Hooves, Equine Build, Survivor
-    View Centaur Details
-    Loxodon
-    Guildmasters' Guide to Ravnica
-
-    Humanoid elephants, loxodons are strong, calm, and wise.
-
-    Racial Traits
-    +2 Constitution, +1 Wisdom, Powerful Build, Loxodon Serenity, Natural Armor, Trunk, Keen Smell
-    View Loxodon Details
-    Minotaur (Legacy)
-    Guildmasters' Guide to Ravnica
-
-    The minotaurs of Ravnica are strong in body, dedication, and courage. They are at home on the battlefield, willing to fight for their various causes.
-
-    Racial Traits
-    +2 Strength, +1 Constitution, Horns, Goring Rush, Hammering Horns, Imposing Presence
-    View Minotaur Details
-    Simic Hybrid
-    Guildmasters' Guide to Ravnica
-
-    The Simic Combine uses magic to transfer the traits of animals into humans, elves, and vedalken.
-
-    Racial Traits
-    +2 Constitution, +1 One Other Ability Score, Darkvision, Animal Enhancement
-    View Simic Hybrid Details
-    Vedalken
-    Guildmasters' Guide to Ravnica
-
-    The blue-skinned vedalken strive for perfection above all else.
-
-    Racial Traits
-    +2 Intelligence, +1 Wisdom, Vedalken Dispassion, Tireless Precision, Partially Amphibious
-    View Vedalken Details
-
-Sword Coast Adventurer's Guide Icon
-Sword Coast Adventurer's Guide
-
-    Feral Tiefling
-    Sword Coast Adventurer's Guide
-
-    To be greeted with stares and whispers, to suffer violence and insult on the street, to see mistrust and fear in every eye: this is the lot of the tiefling.
-
-    Racial Traits
-    +2 Dexterity, +1 Intelligence, Darkvision, Hellish Resistance, Infernal Legacy
-    View Feral Tiefling Details
-
-The Tortle Package Icon
-The Tortle Package
-
-    Tortle (Legacy)
-    The Tortle Package
-
-    What many tortles consider a simple life, others might call a life of adventure. They are nomad survivalists eager to explore the wilderness.
-
-    Racial Traits
-    +2 Strength, +1 Wisdom, Claws, Hold Breath, Natural Armor, Shell Defense, Survival Instinct
-    View Tortle Details
-
-Locathah Rising Icon
-Locathah Rising
-
-    Locathah
-    Locathah Rising
-
-    These resilient and proud fish-folk have endured war, slavery, and mistreatment at the hands of other aquatic creatures.
-
-    Racial Traits
-    +2 Strength, +1 Dexterity, Natural Armor, Observant & Athletic, Leviathan Will, Limited Amphibiousness
-    View Locathah Details
-
-One Grung Above Icon
-One Grung Above
-
-    Grung
-    One Grung Above
-
-    Your grung character has an assortment of inborn abilities, part and parcel of grung nature.
-
-    Racial Traits
-    +2 Dexterity, +1 Constitution, Arboreal Alertness, Amphibious, Poison Immunity, Poisonous Skin, Standing Leap, Water Dependency.
-    View Grung Details
-
-Mordenkainen’s Tome of Foes Icon
-Mordenkainen’s Tome of Foes
-
-    Gith (Legacy)
-    Mordenkainen’s Tome of Foes
-
-    Long ago, the gith rose up to overthrow the mind flayers that held them in servitude, but two factions arose that remain bitter enemies today.
-
-    Racial Traits
-    +1 Intelligence, Githyanki or Githzerai Traits
-    View Gith Details
-
-Volo's Guide to Monsters Icon
-Volo's Guide to Monsters
-
-    Aasimar (Legacy)
-    Volo's Guide to Monsters
-
-    Aasimar are placed in the world to serve as guardians of law and good. Their patrons expect them to strike at evil, lead by example, and further the cause of justice.
-
-    Racial Traits
-    +2 Charisma, Darkvision, Celestial Resistance, Healing Hands, Light Bearer
-    View Aasimar Details
-    Bugbear (Legacy)
-    Volo's Guide to Monsters
-
-    Bugbears feature in the nightmare tales of many races — great, hairy beasts that creep through the shadows as quiet as cats.
-
-    Racial Traits
-    +2 Strength, +1 Dexterity, Darkvision, Long-Limbed, Powerful Build, Sneaky, Surprise Attack
-    View Bugbear Details
-    Firbolg (Legacy)
-    Volo's Guide to Monsters
-
-    Firbolg tribes cloister in remote forest strongholds, preferring to spend their days in quiet harmony with the woods.
-
-    Racial Traits
-    +2 Wisdom, +1 Strength, Firbolg Magic, Hidden Step, Powerful Build, Speech of Beast and Leaf
-    View Firbolg Details
-    Goblin (Legacy)
-    Volo's Guide to Monsters
-
-    Goblins occupy an uneasy place in a dangerous world, and they react by lashing out at any creatures they believe they can bully.
-
-    Racial Traits
-    +2 Dexterity, +1 Constitution, Darkvision, Fury of the Small, Nimble Escape
-    View Goblin Details
-    Hobgoblin (Legacy)
-    Volo's Guide to Monsters
-
-    War is the lifeblood of hobgoblins. Its glories are the dreams that inspire them. Its horrors don’t feature in their nightmares.
-
-    Racial Traits
-    +2 Constitution, +1 Intelligence, Darkvision, Martial Training, Saving Face
-    View Hobgoblin Details
-    Kenku (Legacy)
-    Volo's Guide to Monsters
-
-    Haunted by an ancient crime that robbed them of their wings, the kenku wander the world as vagabonds and burglars who live at the edge of human society.
-
-    Racial Traits
-    +2 Dexterity, +1 Wisdom, Expert Forgery, Kenku Training, Mimicry
-    View Kenku Details
-    Kobold (Legacy)
-    Volo's Guide to Monsters
-
-    Kobolds are typically timid and shy away from conflict, but they are dangerous and vicious if cornered.
-
-    Racial Traits
-    +2 Dexterity, Darkvision, Grovel, Cower, and Beg, Pack Tactics, Sunlight Sensitivity
-    View Kobold Details
-    Lizardfolk (Legacy)
-    Volo's Guide to Monsters
-
-    Lizardfolk possess an alien and inscrutable mindset, their desires and thoughts driven by a different set of basic principles than those of warm-blooded creatures.
-
-    Racial Traits
-    +2 Constitution, +1 Wisdom, Bite, Cunning Artisan, Hold Breath, Hunter's Lore, Natural Armor, Hungry Jaws
-    View Lizardfolk Details
-    Orc (Legacy)
-    Volo's Guide to Monsters
-
-    Orcs live a life that has no place for weakness, and every warrior must be strong enough to take what is needed by force.
-
-    Racial Traits
-    +2 Strength, +1 Constitution, Darkvision, Aggressive, Primal Intuition, Powerful Build
-    View Orc Details
-    Tabaxi (Legacy)
-    Volo's Guide to Monsters
-
-    Hailing from a strange and distant land, wandering tabaxi are catlike humanoids driven by curiosity to collect interesting artifacts, gather tales and stories, and lay eyes on all the world’s wonders.
-
-    Racial Traits
-    +2 Dexterity, +1 Charisma, Darkvision, Feline Agility, Cat's Claws, Cat's Talent
-    View Tabaxi Details
-    Triton (Legacy)
-    Volo's Guide to Monsters
-
-    Long-established guardians of the deep ocean floor, in recent years the noble tritons have become increasingly active in the world above.
-
-    Racial Traits
-    +1 Strength, +1 Constitution, +1 Charisma, Amphibious, Control Air and Water, Emissary of the Sea, Guardians of the Depths
-    View Triton Details
-    Yuan-ti Pureblood (Legacy) }
 classes = {"Artificer": [],
            "Barbarian": [],
            "Bard": [],
@@ -411,7 +232,9 @@ classes = {"Artificer": [],
            "Sorcerer": [],
            "Warlock": [],
            "Wizard": []}
+
 gear = {}
+
 vehicles = {"Galley": [],
             "Keelboat": [],
             "Longship": [],
@@ -440,4 +263,11 @@ vehicles = {"Galley": [],
             "Turtle Ship": [],
             "Tyrant Ship": [],
             "Wasp Ship": []}
+
 magic_items = {}
+
+armor = {"Light Armor": [],
+         "Medium Armor": [],
+         "Heavy Armor": []}
+
+spells = {}
